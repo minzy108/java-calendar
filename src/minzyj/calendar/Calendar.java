263901fb23032fa73579bcbd277dc01c9f76e844
minzy108 <minzy108@gmail.com>
2022-02-11 18:28:47 +0900

    print Calendar ver.4

--- src/minzyj/calendar/Calendar.java
+++ src/minzyj/calendar/Calendar.java
@@ -2,9 +2,9 @@ package minzyj.calendar;
 
 public class Calendar {
 
-	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	
+	private final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
@@ -14,45 +14,37 @@ public class Calendar {
 	}
 
 	public int getMaxDaysOfMonth(int year, int month) {
-		if(isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month - 1];
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month];
 		} else {
-			return MAX_DAYS[month - 1];			
+			return MAX_DAYS[month];
 		}
 	}
 
-	/*
-	 * - 1일의 요일을 입력받는다.
-	 * - 출력한다.
-	 * 
-	 * 년도를 입력하세요.
-	 * YEAR> 2017
-	 * 달을 입력하세요.
-	 * MONTH> 3
-	 * 첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)
-	 * WEEKDAY> WE
-	 */
-
-	public void printCalendar(int year, int month, int weekday) {
-		System.out.printf("   <<%4d년 %2d월>> \n", year, month);
+	public void printCalendar(int year, int month) {
+		System.out.printf("   <<%d년 %d월>> \n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		
+
+		// get weekday automatically
+		int weekday = getWeekDay(year, month, 1);
+
 		// print blank space
-		for(int i = 0; i < weekday; i++) {
+		for (int i = 0; i < weekday; i++) {
 			System.out.print("   ");
 		}
 
 		int maxDay = getMaxDaysOfMonth(year, month);
 		int count = 7 - weekday;
-		int delim = count < 7 ? count : 0;
-		
+		int delim = (count < 7) ? count : 0;
+
 		// print first line
-		for(int i=1; i<= count; i++) {
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%2d ", i);
-		};
-		System.out.println();
+		}
 		
+		System.out.println();
+
 		// print from second line to last line
 		count++;
 		for (int i = count; i <= maxDay; i++) {
@@ -62,7 +54,40 @@ public class Calendar {
 			}
 		}
 		System.out.println();
-		
+
+	}
+
+	private int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 3; // 1970/Jan/1st/Thursday
+
+		int count = 0;
+
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+
+//		System.out.println(count);
+		for (int i = 1; i < month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+
+		count += day;
+
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+		return weekday;
+	}
+
+	// simple test code here
+	public static void main(String[] args) {
+		Calendar cal = new Calendar();
+		System.out.println(cal.getWeekDay(1970, 1, 1) == 3);
+		System.out.println(cal.getWeekDay(1971, 1, 1) == 4);
+		System.out.println(cal.getWeekDay(1972, 1, 1) == 5);
+		System.out.println(cal.getWeekDay(1973, 1, 1) == 0);
+		System.out.println(cal.getWeekDay(1974, 1, 1) == 1);
 
 	}
 
--- src/minzyj/calendar/Prompt.java
+++ src/minzyj/calendar/Prompt.java
@@ -35,7 +35,6 @@ public class Prompt {
 
 		int month = 1;
 		int year = 2017;
-		int weekday = 0;
 		
 		while (true) {
 			System.out.println("년도를 입력하세요. (exit: -1)");
@@ -53,11 +52,8 @@ public class Prompt {
 				continue;
 			}
 			
-			System.out.println("첫째 날의 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
-			String str_weekday = scan.next();
-			weekday = parseDay(str_weekday);
 			
-			cal.printCalendar(year, month, weekday);
+			cal.printCalendar(year, month);
 		}
 
 		System.out.println("BYE");
