192d5223029d63e2636181bc0d53eec08c74d5a4
minzy108 <minzy108@gmail.com>
2022-02-11 15:53:38 +0900

    Implement find max days of the month

--- src/minzyj/calendar/Calendar.java
+++ src/minzyj/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package minzyj.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -10,6 +12,18 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	
+		
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("달을 입력");
+		int month = scan.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d까지 있습니다. \n", month, maxDays[month - 1]);
+		scan.close();
 
 	}
 
