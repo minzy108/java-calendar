b2adba350fab7a707371fb884869f87dd5d44f8b
minzy108 <minzy108@gmail.com>
2022-02-11 16:54:51 +0900

    Print Calendar Ver.2

--- src/minzyj/calendar/Calendar.java
+++ src/minzyj/calendar/Calendar.java
@@ -11,18 +11,21 @@ public class Calendar {
 	}
 
 	/*
-	 * - 월을 입력하면 해당월의 달력을 출력한다. - 달력의 모양은 1단계에서 작성한 모양으로 만든다. - 1일은 일요일로 정해도 무방하다. -
-	 * -1을 입력받기 전까지 반복 입력받는다. - 프롬프트를 출력한다.
+	 * - 월을 입력하면 해당월의 달력을 출력한다.
+	 * - 달력의 모양은 1단계에서 작성한 모양으로 만든다. 
+	 * - 1일은 일요일로 정해도 무방하다. -
+	 * -1을 입력받기 전까지 반복 입력받는다. 
+	 * - 프롬프트를 출력한다.
 	 */
 
 	public void printCalendar(int year, int month) {
 		System.out.printf("   <<%4d년 %2d월>> \n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		
+
 		int maxDay = getMaxDaysOfMonth(month);
-		
-		for(int i = 1; i <= maxDay; i++) {
+
+		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d ", i);
 			if (i % 7 == 0) {
 				System.out.println();
@@ -30,7 +33,7 @@ public class Calendar {
 		}
 		System.out.println();
 		System.out.println();
-		
+
 //		System.out.println(" 1  2  3  4  5  6  7");
 //		System.out.println(" 8  9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
