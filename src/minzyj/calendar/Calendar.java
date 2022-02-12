b4dd00fd3b9f20d4f3d02506f7fdeddaab920be2
minzy108 <minzy108@gmail.com>
2022-02-12 17:27:47 +0900

    print Calendar ver.5(add register plan, search plan)

--- src/minzyj/calendar/Calendar.java
+++ src/minzyj/calendar/Calendar.java
@@ -1,10 +1,40 @@
 package minzyj.calendar;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
 public class Calendar {
 
 	private final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+	
+	private HashMap <Date, String> planMap;
+	
+	public Calendar() {
+		planMap = new HashMap<Date, String>();
+	}
+	
+	/**
+	 * 
+	 * @param date ex: "2017-06-20"
+	 * @param plan
+	 * @throws ParseException 
+	 */
+	
+	public void registerPlan(String strDate, String plan) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		planMap.put(date, plan);
+	}
+	
+	public String searchPlan(String strDate) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		String plan = planMap.get(date);
+		return plan;
+	}
+	
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
@@ -59,7 +89,7 @@ public class Calendar {
 
 	private int getWeekDay(int year, int month, int day) {
 		int syear = 1970;
-		final int STANDARD_WEEKDAY = 3; // 1970/Jan/1st/Thursday
+		final int STANDARD_WEEKDAY = 4; // 1970/Jan/1st/Thursday
 
 		int count = 0;
 
@@ -74,20 +104,23 @@ public class Calendar {
 			count += delta;
 		}
 
-		count += day;
+		count += day - 1;
 
 		int weekday = (count + STANDARD_WEEKDAY) % 7;
 		return weekday;
 	}
 
 	// simple test code here
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Calendar cal = new Calendar();
-		System.out.println(cal.getWeekDay(1970, 1, 1) == 3);
-		System.out.println(cal.getWeekDay(1971, 1, 1) == 4);
-		System.out.println(cal.getWeekDay(1972, 1, 1) == 5);
-		System.out.println(cal.getWeekDay(1973, 1, 1) == 0);
-		System.out.println(cal.getWeekDay(1974, 1, 1) == 1);
+		System.out.println(cal.getWeekDay(1970, 1, 1) == 4);
+		System.out.println(cal.getWeekDay(1971, 1, 1) == 5);
+		System.out.println(cal.getWeekDay(1972, 1, 1) == 6);
+		System.out.println(cal.getWeekDay(1973, 1, 1) == 1);
+		System.out.println(cal.getWeekDay(1974, 1, 1) == 2);
+		
+		cal.registerPlan("2017-06-23", "meal");
+		System.out.println(cal.searchPlan("2017-06-23"));
 
 	}
 
--- src/minzyj/calendar/Prompt.java
+++ src/minzyj/calendar/Prompt.java
@@ -1,68 +1,94 @@
 package minzyj.calendar;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
-	
-	/*
-	 * @param week 요일명
-	 * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
-	 * */
-	
-	public int parseDay(String week) {
-		if(week.equals("SU")) {
-			return 0;
-		} else if(week.equals("MO")) {
-			return 1;
-		} else if(week.equals("TU")) {
-			return 2;
-		} else if(week.equals("WE")) {
-			return 3;
-		} else if(week.equals("TH")) {
-			return 4;
-		} else if(week.equals("FR")) {
-			return 5;
-		} else if(week.equals("SA")) {
-			return 6;
-		} else {
-			return 0;
-		}
+
+	public void printMenu() {
+		System.out.println("+------------------+");
+		System.out.println("| 1. 일정 등록 ");
+		System.out.println("| 2. 일정 검색 ");
+		System.out.println("| 3. 달력 보기 ");
+		System.out.println("| h. 도움말 q. 종료 ");
+		System.out.println("+-------------------+ ");
 	}
 
-	public void runPrompt() {
+	public void runPrompt() throws ParseException {
+		printMenu();
+
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		int month = 1;
-		int year = 2017;
-		
 		while (true) {
-			System.out.println("년도를 입력하세요. (exit: -1)");
-			System.out.print("YEAR> ");
-			year = scan.nextInt();
-			if(year == -1) {
+			System.out.println("명령 (1, 2, 3, h, q)");
+			String cmd = scan.next();
+			if (cmd.equals("1")) {
+				cmdRegister(scan, cal);
+			} else if (cmd.equals("2")) {
+				cmdSearch(scan, cal);
+			} else if (cmd.equals("3")) {
+				cmdCal(scan, cal);
+			} else if (cmd.equals("h")) {
+				printMenu();
+			} else if (cmd.equals("q")) {
 				break;
 			}
-			
-			System.out.println("달을 입력하세요.");
-			System.out.print("MONTH> ");
-			month = scan.nextInt();
-			if (month > 12 || month < 1) {
-				System.out.println("잘못된 입력입니다.");
-				continue;
-			}
-			
-			
-			cal.printCalendar(year, month);
+
 		}
 
-		System.out.println("BYE");
-
+		System.out.println("GoodBye");
 		scan.close();
 
 	}
 
-	public static void main(String[] args) {
+	private void cmdRegister(Scanner scan, Calendar cal) throws ParseException {
+		System.out.println("[새 일정 등록]");
+		System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd)");
+		String date = scan.next();
+		String text = "";
+		scan.nextLine();
+		System.out.println("일정을 입력해 주세요.");
+		text = scan.nextLine();
+		cal.registerPlan(date, text);
+	}
+
+	private void cmdCal(Scanner scan, Calendar cal) {
+		int month = 1;
+		int year = 2017;
+
+		System.out.println("년도를 입력하세요. (exit: -1)");
+		System.out.print("YEAR> ");
+		year = scan.nextInt();
+
+		System.out.println("달을 입력하세요.");
+		System.out.print("MONTH> ");
+		month = scan.nextInt();
+		if (month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.");
+			return;
+		}
+
+		cal.printCalendar(year, month);
+
+	}
+
+	private void cmdSearch(Scanner scan, Calendar cal) {
+		System.out.println("[일정 검색]");
+		System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd)");
+		String date = scan.next();
+		String plan = "";
+		try {
+			plan = cal.searchPlan(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.err.println("일정 검색 중 오류가 발생했습니다.");
+		}
+		System.out.println(plan);
+
+	}
+
+	public static void main(String[] args) throws ParseException {
 
 		// 쉘 실행
 
